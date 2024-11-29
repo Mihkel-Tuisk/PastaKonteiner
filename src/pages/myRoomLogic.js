871081c455d01e70f805ac02f354f16a98f6b5c8
Mihkel-Tuisk <mihkel.tuisk@ut.ee>
2024-11-29 19:08:13 +0200

    test

--- src/pages/myRoomLogic.js
+++ src/pages/myRoomLogic.js
@@ -104,43 +104,6 @@ async function updateDOM() {
   }
 }
 
-async function mineTuppaBtn() {
-  const roomID = document.getElementById("toakood").value;
-  if (!roomID) {
-    teavita("Palun sisesta toakood!");
-    return;
-  }
-
-  const pastadekonteiner = document.getElementById("teisteToad");
-  if (!pastadekonteiner) {
-    teavita("teisteToad elementi ei leitud!");
-    return;
-  }
-
-  pastadekonteiner.innerHTML = "";
-
-  async function addDOMcategory(roomID) {
-    const roomText = document.createElement("input");
-    const roomCode = document.createElement("p");
-
-    const roomTextQuery = await getRoomText(roomID);
-    if (roomTextQuery.error != null) {
-      teavita(roomTextQuery.error);
-      return;
-    }
-
-    roomText.value = roomTextQuery.text;
-    roomText.id = roomID;
-    roomText.readOnly = true;
-    roomCode.textContent = roomID;
-
-    pastadekonteiner.appendChild(roomCode);
-    pastadekonteiner.appendChild(roomText);
-  }
-
-  await addDOMcategory(roomID);
-}
-
 window.addEventListener('load', async function() {
   await updateDOM();
 });
